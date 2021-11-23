0c18f40fc1e3a34d78838d704efa5d8339309973
OziArantha <93304800+OziArantha@users.noreply.github.com>
2021-11-23 20:36:15 +0700

    Add files via upload
    buat class minuman

---
+++ Minuman.java
@@ -0,0 +1,62 @@
+public class Minuman
+{
+    private int rasa; 
+    private int budgetMinuman;
+    private int ukuran; 
+    private int suhu;
+    private int jenisMinuman;
+    private int asalMinuman;
+    
+    public Minuman()
+    {
+        
+    }
+    
+    public void setBudgetMinuman(int budget)
+    {
+        this.budgetMinuman=budget;
+    }
+    
+    public void setRasa(int rasa)
+    {
+        this.rasa=rasa;
+    }
+    
+    public void setUkuran(int ukuran)
+    {
+        this.ukuran=ukuran;
+    }
+    
+    public void setSuhu(int suhu)
+    {
+        this.suhu=suhu;
+    }
+    
+    public void setJenisMinuman(int jenis)
+    {
+        this.jenisMinuman=jenis;
+    }
+    
+    public void setAsalMinuman(int asal)
+    {
+        this.asalMinuman=asal;
+    }
+     
+    public String[] getMinuman()
+    {
+        String[] minuman = new String[7];
+        
+        return minuman;
+    }
+}
+
+
+
+    
+   
+  
+
+    
+    
+
+
